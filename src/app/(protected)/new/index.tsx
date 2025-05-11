62c094c3a05d3006d22bb61cc052ebc9554d5ef7
kaisar Hameed <kaisersofi.ks2515@gmail.com>
2025-05-11 17:33:50 +0530

    New Post Modal

---
+++ src/app/(protected)/new/index.tsx
@@ -0,0 +1,5 @@
+import { NewPost as NewPostScreen } from "@/screens/new-post";
+
+export default function NewPost() {
+  return <NewPostScreen />;
+}
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,5 +1,6 @@
 import { PostListItem } from "@/components/post-list-item";
 import { dummyPosts } from "@/dummy-data";
+import { Link } from "expo-router";
 import { FlatList } from "react-native";
 
 export function Home() {
@@ -8,6 +9,11 @@ export function Home() {
       showsVerticalScrollIndicator={false}
       data={dummyPosts}
       renderItem={({ item }) => <PostListItem post={item} />}
+      ListHeaderComponent={() => (
+        <Link href="/new" className="text-blue-500 p-4 text-center text-3xl">
+          New Post
+        </Link>
+      )}
     />
   );
 }
